c82db91bcffa00445c7b2adf943f64c3c0be4fc2
Sasha Lambert <sasha.lambert@etu.univ-lyon1.fr>
2026-03-13 17:07:05 +0100

    Correction bug affichage public_road

--- Code/templates/Roadtrips/public_roadtrips.php
+++ Code/templates/Roadtrips/public_roadtrips.php
@@ -116,11 +116,18 @@ $isAdmin = $currentUser && isset($currentUser->role) && $currentUser->role === '
                                 <i class="material-icons">picture_as_pdf</i>
                             </a>
 
-                            <a class="action-btn view"
-                                href="<?= $this->Url->build(['controller' => 'Favorites', 'action' => 'view', $favorite->id]) ?>"
-                                title="Voir le favori">
-                                <i class="material-icons">favorite</i>
-                            </a>
+                            <?php if ($currentUser): ?>
+                                <?php
+                                $isFavorited = in_array($rt->id, $favorisIds);
+                                ?>
+
+                                <a class="action-btn <?= $isFavorited ? 'favorited' : '' ?>"
+                                   href="<?= $this->Url->build(['controller' => 'Favorites', 'action' => $isFavorited ? 'delete' : 'add', $rt->id]) ?>"
+                                   title="<?= $isFavorited ? 'Retirer des favoris' : 'Ajouter aux favoris' ?>"
+                                   style="<?= $isFavorited ? 'color: red;' : '' ?>">
+                                    <i class="material-icons"><?= $isFavorited ? 'favorite' : 'favorite_border' ?></i>
+                                </a>
+                            <?php endif; ?>
 
                             <button type="button"
                                     class="action-btn btn-open-avis"
